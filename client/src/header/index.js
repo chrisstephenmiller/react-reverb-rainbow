2edd79ed7bc94f16887ac5d9e7decd9054e753f9
chrisstpehenmiller <csmiller4567@gmail.com>
2018-04-19 19:20:47 -0500

    header animtation almost done?

--- client/src/header/index.js
+++ client/src/header/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './header.css'
 
 // const colors = [`red`, `orange`, `yellow`, `green`, `blue`, `purple`]
 
-const Header = () => {
-  return (
-    <div id="header">
-      <div id="header-text">
-        <span>REVERB RAINBOW</span><span id="author">made by chris miller</span>
+class Header extends Component {
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = event => {
+    const header = document.getElementById('header')
+    window.pageYOffset > 300 ? header.classList.add('hide') : header.classList.remove('hide')
+  }
+
+  render() {
+    return (
+      <div id="header">
+        <div id="header-text">
+        <span>REVERB RAINBOW </span><span id="author"> - made by chris miller</span>
+        </div>
       </div>
-      </div>
-  );
+    );
+  }
 }
 
 export default Header;
--- server/index.js
+++ server/index.js
@@ -6,23 +6,15 @@ const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
 const app = express();
 
-//logging middleware
 app.use(volleyball);
 
-//body parsing middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//static middleware
-app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.use('/api', require('./api')); // include our routes!
+app.use('/api', require('./api'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/public/index.html'));
-}); // Send index.html for any other requests
-
-//error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(err.status || 500).send(err.message || 'Internal server error');
@@ -31,7 +23,7 @@ app.use((err, req, res, next) => {
 const { db, Listing } = require('./db')
 const PORT = 3001
 
-db.sync({ force: false }) // if you update your db schemas, make sure you drop the tables first and then recreate them
+db.sync({ force: false })
     .then(() => {
         console.log('db synced')
         app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
